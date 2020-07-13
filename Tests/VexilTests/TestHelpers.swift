21a36f825c74507a5f9b2bd9678d1aa653c590b7
Rob Amos <bok-@users.noreply.github.com>
2020-07-13 14:45:11 +1000

    Cleaned up Swiftlint warnings

--- Tests/VexilTests/TestHelpers.swift
+++ Tests/VexilTests/TestHelpers.swift
@@ -7,11 +7,11 @@
 
 import XCTest
 
-public func AssertNoThrow (file: StaticString = #file, line: UInt = #line, _ expression: () throws -> ()) {
+public func AssertNoThrow (file: StaticString = #file, line: UInt = #line, _ expression: () throws -> Void) {
     XCTAssertNoThrow(try expression(), file: file, line: line)
 }
 
-public func AssertThrows<E> (error: E, file: StaticString = #file, line: UInt = #line, _ expression: () throws -> ()) where E: Equatable {
+public func AssertThrows<E> (error: E, file: StaticString = #file, line: UInt = #line, _ expression: () throws -> Void) where E: Equatable {
     var result: E?
     XCTAssertThrowsError(try expression(), file: file, line: line) { thrownError in
         result = thrownError as? E
@@ -20,11 +20,10 @@ public func AssertThrows<E> (error: E, file: StaticString = #file, line: UInt = 
 }
 
 @discardableResult
-public func AssertThrows (file: StaticString = #file, line: UInt = #line, _ expression: () throws -> ()) -> Swift.Error? {
+public func AssertThrows (file: StaticString = #file, line: UInt = #line, _ expression: () throws -> Void) -> Swift.Error? {
     var result: Swift.Error?
     XCTAssertThrowsError(try expression(), file: file, line: line) { thrownError in
         result = thrownError
     }
     return result
 }
-
