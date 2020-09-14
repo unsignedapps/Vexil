4d64e11829f548e2a26603fed7231036591d3ee2
Rob Amos <bok-@users.noreply.github.com>
2020-09-14 23:29:02 +1000

    Added additional documentation for the coversage

--- Sources/Vexil/Sources/NSUbiquitousKeyValueStore+FlagValueSource.swift
+++ Sources/Vexil/Sources/NSUbiquitousKeyValueStore+FlagValueSource.swift
@@ -14,10 +14,12 @@ import Foundation
 ///
 extension NSUbiquitousKeyValueStore: FlagValueSource {
 
+    /// The name of the Flag Value Source
     public var name: String {
         return "NSUbiquitousKeyValueStore\(self == NSUbiquitousKeyValueStore.default ? ".default" : "")"
     }
 
+    /// Fetch values for the specified key
     public func flagValue<Value>(key: String) -> Value? where Value: FlagValue {
 
         guard
@@ -28,6 +30,7 @@ extension NSUbiquitousKeyValueStore: FlagValueSource {
         return Value(boxedFlagValue: boxed)
     }
 
+    /// Sets the value for the specified key
     public func setFlagValue<Value>(_ value: Value?, key: String) throws where Value: FlagValue {
         guard let value = value else {
             self.removeObject(forKey: key)
@@ -43,6 +46,7 @@ extension NSUbiquitousKeyValueStore: FlagValueSource {
 
     private static let didChangeInternallyNotification = NSNotification.Name(rawValue: "NSUbiquitousKeyValueStore.didChangeExternallyNotification")
 
+    /// A Publisher that emits events when the flag values it manages changes
     public var valuesDidChange: AnyPublisher<Void, Never>? {
         return Publishers.Merge(
                 NotificationCenter.default.publisher(for: Self.didChangeExternallyNotification, object: self).map { _ in () },
--- Sources/Vexil/Sources/UserDefaults+FlagValueSource.swift
+++ Sources/Vexil/Sources/UserDefaults+FlagValueSource.swift
@@ -15,10 +15,12 @@ import Foundation
 ///
 extension UserDefaults: FlagValueSource {
 
+    /// The name of the Flag Value Source
     public var name: String {
         return "UserDefaults\(self == UserDefaults.standard ? ".standard" : "")"
     }
 
+    /// Fetch values for the specified key
     public func flagValue<Value>(key: String) -> Value? where Value: FlagValue {
 
         guard
@@ -29,6 +31,7 @@ extension UserDefaults: FlagValueSource {
         return Value(boxedFlagValue: boxed)
     }
 
+    /// Sets the value for the specified key
     public func setFlagValue<Value>(_ value: Value?, key: String) throws where Value: FlagValue {
         guard let value = value else {
             self.removeObject(forKey: key)
@@ -41,6 +44,7 @@ extension UserDefaults: FlagValueSource {
 
     #if !os(Linux)
 
+    /// A Publisher that emits events when the flag values it manages changes
     public var valuesDidChange: AnyPublisher<Void, Never>? {
         return NotificationCenter.default.publisher(for: UserDefaults.didChangeNotification)
             .map { _ in () }
