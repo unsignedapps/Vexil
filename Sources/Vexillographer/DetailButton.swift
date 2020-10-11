a8b5d3d530c90598f5a50f239a0cbf0318eabb59
Rod Brown <rodney.brown6@icloud.com>
2020-10-12 08:52:43 +1100

    Improve Detail Button behaviour in iOS forms

--- Sources/Vexillographer/DetailButton.swift
+++ Sources/Vexillographer/DetailButton.swift
@@ -14,7 +14,12 @@ struct DetailButton: View {
     // MARK: - Properties
 
     let hasChanges: Bool
+
     @Binding var showDetail: Bool
+
+    @State private var size = CGSize.zero
+
+    @State private var isDraggingInside = false
 
 
     // MARK: - View
@@ -22,11 +27,36 @@ struct DetailButton: View {
 #if os(iOS)
 
     var body: some View {
-        Button (
-            action: {},
-            label: { Image(systemName: self.hasChanges ? "info.circle.fill" : "info.circle") }
-        )
-            .onTapGesture { self.showDetail.toggle() }
+        Image(systemName: self.hasChanges ? "info.circle.fill" : "info.circle")
+            .imageScale(.large)
+            .foregroundColor(.accentColor)
+            .opacity(self.isDraggingInside ? 0.3 : 1)
+            .animation(self.isDraggingInside ? .easeOut(duration: 0.15) : .easeIn(duration: 0.2), value: self.isDraggingInside)
+            .background(
+                GeometryReader { proxy in
+                    Color.clear
+                        .preference(key: SizePreferenceKey.self, value: proxy.size)
+                }
+            )
+            .onPreferenceChange(SizePreferenceKey.self) { size in
+                self.size = size
+            }
+            .gesture(selectionGesture)
+    }
+
+    private var selectionGesture: some Gesture {
+        DragGesture(minimumDistance: 0)
+            .onChanged { data in
+                self.isDraggingInside = CGRect(origin: .zero, size: size)
+                    .insetBy(dx: -10, dy: -10)
+                    .contains(data.location)
+            }
+            .onEnded { _ in
+                if self.isDraggingInside {
+                    self.showDetail.toggle()
+                    self.isDraggingInside = false
+                }
+            }
     }
 
 #elseif os(macOS)
@@ -40,3 +70,12 @@ struct DetailButton: View {
 }
 
 #endif
+
+private struct SizePreferenceKey: PreferenceKey {
+    typealias Value = CGSize
+    static var defaultValue: Value = .zero
+
+    static func reduce(value: inout Value, nextValue: () -> Value) {
+        value = nextValue()
+    }
+}
