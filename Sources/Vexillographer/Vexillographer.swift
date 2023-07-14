61620b42f1b5ee54e0b73469369a242b3b92e020
Pat Brown <pat.brown@anz.com>
2023-07-15 08:59:47 +1000

    Fix test

--- Sources/Vexillographer/Vexillographer.swift
+++ Sources/Vexillographer/Vexillographer.swift
@@ -16,6 +16,8 @@
 import SwiftUI
 import Vexil
 
+#if os(macOS) && compiler(>=5.3.1)
+
 /// A SwiftUI View that allows you to easily edit the flag
 /// structure in a provided FlagValueSource.
 @available(OSX 11.0, iOS 13.0, watchOS 7.0, tvOS 13.0, *)
@@ -23,13 +25,8 @@ public struct Vexillographer<RootGroup>: View where RootGroup: FlagContainer {
 
     // MARK: - Properties
 
-#if os(macOS) && compiler(>=5.3.1)
     @ObservedObject
     var manager: FlagValueManager<RootGroup>
-#else
-    @State
-    var manager: FlagValueManager<RootGroup>
-#endif
 
     // MARK: - Initialisation
 
@@ -40,13 +37,10 @@ public struct Vexillographer<RootGroup>: View where RootGroup: FlagContainer {
     ///   - source:             An optional `FlagValueSource` for editing the flag values in. If `nil` the flag values are displayed read-only
     ///
     public init(flagPole: FlagPole<RootGroup>, source: FlagValueSource?) {
-        self.manager = FlagValueManager(flagPole: flagPole, source: source)
+        self._manager = ObservedObject(wrappedValue: FlagValueManager(flagPole: flagPole, source: source))
     }
 
-
     // MARK: - Body
-
-#if os(macOS) && compiler(>=5.3.1)
 
     public var body: some View {
         List(self.manager.allItems(), id: \.id, children: \.childLinks) { item in
@@ -61,8 +55,31 @@ public struct Vexillographer<RootGroup>: View where RootGroup: FlagContainer {
             }
         }
     }
+}
 
 #else
+
+/// A SwiftUI View that allows you to easily edit the flag
+/// structure in a provided FlagValueSource.
+@available(OSX 11.0, iOS 13.0, watchOS 7.0, tvOS 13.0, *)
+public struct Vexillographer<RootGroup>: View where RootGroup: FlagContainer {
+
+    // MARK: - Properties
+
+    @State
+    var manager: FlagValueManager<RootGroup>
+
+    // MARK: - Initialisation
+
+    /// Initialises a new `Vexillographer` instance with the provided FlagPole and source
+    ///
+    /// - Parameters;
+    ///   - flagPole:           A `FlagPole` instance manages the flag and source hierarchy we want to display
+    ///   - source:             An optional `FlagValueSource` for editing the flag values in. If `nil` the flag values are displayed read-only
+    ///
+    public init(flagPole: FlagPole<RootGroup>, source: FlagValueSource?) {
+        self._manager = State(wrappedValue: FlagValueManager(flagPole: flagPole, source: source))
+    }
 
     public var body: some View {
         ForEach(self.manager.allItems(), id: \.id) { item in
@@ -70,8 +87,8 @@ public struct Vexillographer<RootGroup>: View where RootGroup: FlagContainer {
         }
         .environmentObject(self.manager)
     }
+}
 
 #endif
-}
 
 #endif
