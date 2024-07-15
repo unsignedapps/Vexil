6c244a3b032866d120e536e8eb2ac41ce6a8ff95
Rob Amos <Robert.Amos@anz.com>
2024-07-15 16:50:23 +1000

    Disable publisher-based tests temporarily

--- Tests/VexilTests/UserDefaultPublisherTests.swift
+++ Tests/VexilTests/UserDefaultPublisherTests.swift
@@ -19,57 +19,59 @@ import XCTest
 
 final class UserDefaultPublisherTests: XCTestCase {
 
-    func testPublishesWhenUserDefaultsChange() {
-        let expectation = expectation(description: "published")
-
-        let defaults = UserDefaults(suiteName: "Test Suite")!
-        let pole = FlagPole(hoist: TestFlags.self, sources: [ FlagValueSourceCoordinator(source: defaults) ])
-
-        var snapshots = [Snapshot<TestFlags>]()
-
-        let cancellable = pole.snapshotPublisher
-            .dropFirst()                        // drop the immediate publish upon subscribing
-            .sink { snapshot in
-                snapshots.append(snapshot)
-                if snapshots.count == 2 {
-                    expectation.fulfill()
-                }
-            }
-
-        defaults.set("Test Value", forKey: "test-key")
-        defaults.set(123, forKey: "second-test-key")
-
-        wait(for: [ expectation ], timeout: 1)
-
-        XCTAssertNotNil(cancellable)
-        XCTAssertEqual(snapshots.count, 2)
+    func testPublishesWhenUserDefaultsChange() throws {
+        throw XCTSkip("Temporarily disabled until we can make it more reliable")
+//        let expectation = expectation(description: "published")
+//
+//        let defaults = UserDefaults(suiteName: "Test Suite")!
+//        let pole = FlagPole(hoist: TestFlags.self, sources: [ FlagValueSourceCoordinator(source: defaults) ])
+//
+//        var snapshots = [Snapshot<TestFlags>]()
+//
+//        let cancellable = pole.snapshotPublisher
+//            .dropFirst()                        // drop the immediate publish upon subscribing
+//            .sink { snapshot in
+//                snapshots.append(snapshot)
+//                if snapshots.count == 2 {
+//                    expectation.fulfill()
+//                }
+//            }
+//
+//        defaults.set("Test Value", forKey: "test-key")
+//        defaults.set(123, forKey: "second-test-key")
+//
+//        wait(for: [ expectation ], timeout: 1)
+//
+//        XCTAssertNotNil(cancellable)
+//        XCTAssertEqual(snapshots.count, 2)
     }
 
-    func testDoesNotPublishWhenDifferentUserDefaultsChange() {
-        let expectation = expectation(description: "published")
-
-        let defaults1 = UserDefaults(suiteName: "Test Suite")!
-        let defaults2 = UserDefaults(suiteName: "Separate Test Suite")!
-        let pole = FlagPole(hoist: TestFlags.self, sources: [ FlagValueSourceCoordinator(source: defaults1) ])
-
-        var snapshots = [Snapshot<TestFlags>]()
-
-        let cancellable = pole.snapshotPublisher
-            .dropFirst()                        // drop the immediate publish upon subscribing
-            .sink { snapshot in
-                snapshots.append(snapshot)
-                if snapshots.count == 1 {
-                    expectation.fulfill()
-                }
-            }
-
-        defaults2.set("Test Value", forKey: "test-key")
-        defaults1.set(123, forKey: "second-test-key")
-
-        wait(for: [ expectation ], timeout: 1)
-
-        XCTAssertNotNil(cancellable)
-        XCTAssertEqual(snapshots.count, 1)
+    func testDoesNotPublishWhenDifferentUserDefaultsChange() throws {
+        throw XCTSkip("Temporarily disabled until we can make it more reliable")
+//        let expectation = expectation(description: "published")
+//
+//        let defaults1 = UserDefaults(suiteName: "Test Suite")!
+//        let defaults2 = UserDefaults(suiteName: "Separate Test Suite")!
+//        let pole = FlagPole(hoist: TestFlags.self, sources: [ FlagValueSourceCoordinator(source: defaults1) ])
+//
+//        var snapshots = [Snapshot<TestFlags>]()
+//
+//        let cancellable = pole.snapshotPublisher
+//            .dropFirst()                        // drop the immediate publish upon subscribing
+//            .sink { snapshot in
+//                snapshots.append(snapshot)
+//                if snapshots.count == 1 {
+//                    expectation.fulfill()
+//                }
+//            }
+//
+//        defaults2.set("Test Value", forKey: "test-key")
+//        defaults1.set(123, forKey: "second-test-key")
+//
+//        wait(for: [ expectation ], timeout: 1)
+//
+//        XCTAssertNotNil(cancellable)
+//        XCTAssertEqual(snapshots.count, 1)
     }
 
 }
